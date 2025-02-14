5e61b79af9f1051bf533ad88d77f2d158b97eb5d
Nicolas Santana <sergio.n.santana@gmail.com>
2025-02-13 18:07:13 -0600

    Added support for dynamic IP address configuration with DHCP.

--- simple_tcp_echo.c
+++ simple_tcp_echo.c
@@ -8,8 +8,10 @@
 #include "simple_tcp_echo.h"
 #include "lwip/opt.h"
 
+#include "lwip/dhcp.h"
 #include "lwip/sys.h"
 #include "lwip/netifapi.h"
+#include "lwip/prot/dhcp.h"
 #include "lwip/netif.h"
 #include "lwip/tcpip.h"
 #include "lwip/sockets.h"
@@ -33,8 +35,14 @@
 #endif
 /* Number of network physical interfaces used */
 #define BOARD_PHY_COUNT 1
-
+/* Max number of TCP clients supported at the same time. */
 #define TCP_SERVER_CONNECTIONS_MAX 2
+/* Use DHCP instead of static IP */
+#define USE_DHCP (0)
+/*! @brief Priority of the thread which prints DHCP info. */
+#define PRINT_THREAD_PRIO DEFAULT_THREAD_PRIO
+/*! @brief Stack size of the thread which prints DHCP info. */
+#define PRINT_THREAD_STACKSIZE 512
 
 /*******************************************************************************
  * Prototypes
@@ -52,11 +60,18 @@ static int ip_port_to_sockaddr(const char *ip_str,
                                int port,
                                struct sockaddr_in *ipv4,
                                struct sockaddr_in6 *ipv6);
+/* Called by the netif module to show link status */
+static void linkStatusCallback(struct netif *netif_,
+		                       netif_nsc_reason_t reason,
+							   const netif_ext_callback_args_t *args);
+/* Print DHCP */
+static void print_dhcp_state(void *arg);
 
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 static phy_handle_t phyHandle;
+static netif_ext_callback_t linkStatusCallbackInfo;
 
 /*******************************************************************************
  * Interface Code
@@ -105,11 +120,18 @@ static void network_interface_init(void *arg)
 
     tcpip_init(NULL, NULL);
 
+    LOCK_TCPIP_CORE();
+    netif_add_ext_callback(&linkStatusCallbackInfo, linkStatusCallback);
+    UNLOCK_TCPIP_CORE();
+#if USE_DHCP
+    netifapi_netif_add(&s_netif0, NULL, NULL, NULL, &enet0_config, EXAMPLE_NETIF_INIT_FN, tcpip_input);
+#else
     IP4_ADDR(&netif0_ipaddr, configIP_ADDR0, configIP_ADDR1, configIP_ADDR2, configIP_ADDR3);
     IP4_ADDR(&netif0_netmask, configNET_MASK0, configNET_MASK1, configNET_MASK2, configNET_MASK3);
     IP4_ADDR(&netif0_gw, configGW_ADDR0, configGW_ADDR1, configGW_ADDR2, configGW_ADDR3);
     netifapi_netif_add(&s_netif0, &netif0_ipaddr, &netif0_netmask, &netif0_gw, &enet0_config, EXAMPLE_NETIF_INIT_FN,
                        tcpip_input);
+#endif
     netifapi_netif_set_up(&s_netif0);
 
     /*
@@ -118,10 +140,11 @@ static void network_interface_init(void *arg)
      */
     netifapi_netif_set_default(&s_netif0);
 
+#if !USE_DHCP
     LOCK_TCPIP_CORE();
     netif_create_ip6_linklocal_address(&s_netif0, 1);
     UNLOCK_TCPIP_CORE();
-
+#endif
     struct netif *netif_array[BOARD_PHY_COUNT];
     netif_array[0] = &s_netif0;
 
@@ -130,6 +153,15 @@ static void network_interface_init(void *arg)
         PRINTF("PHY Auto-negotiation failed. Please check the cable connection and link partner setting.\r\n");
     }
 
+#if USE_DHCP
+    PRINTF("Enabling DHCP!\r\n");
+    netifapi_dhcp_start(&s_netif0);
+
+    if (sys_thread_new("print_dhcp", print_dhcp_state, &s_netif0, PRINT_THREAD_STACKSIZE, PRINT_THREAD_PRIO) == NULL)
+    {
+        LWIP_ASSERT("stack_init(): Task creation failed.", 0);
+    }
+#else
     PRINTF("************************************************\r\n");
     PRINTF(" Interface name   : %s%d\r\n", netif_default->name, netif_default->num);
     PRINTF(" IPv4 Address     : %s\r\n", ip4addr_ntoa(netif_ip4_addr(netif_default)));
@@ -147,6 +179,7 @@ static void network_interface_init(void *arg)
     PRINTF("************************************************\r\n");
 
     STE_start_tcp_echo_server();
+#endif
     vTaskDelete(NULL);
 }
 
@@ -354,3 +387,132 @@ static int ip_port_to_sockaddr(const char *ip_str,
     return af;
 }
 
+/*!
+ * @brief Link status callback - prints link status events.
+ */
+static void linkStatusCallback(struct netif *netif_, netif_nsc_reason_t reason, const netif_ext_callback_args_t *args)
+{
+    if (reason != LWIP_NSC_LINK_CHANGED)
+        return;
+
+    PRINTF("[LINK STATE] netif=%d, state=%s", netif_->num, args->link_changed.state ? "up" : "down");
+
+    if (args->link_changed.state)
+    {
+        char *speedStr;
+        switch (ethernetif_get_link_speed(netif_))
+        {
+            case kPHY_Speed10M:
+                speedStr = "10M";
+                break;
+            case kPHY_Speed100M:
+                speedStr = "100M";
+                break;
+            case kPHY_Speed1000M:
+                speedStr = "1000M";
+                break;
+            default:
+                speedStr = "N/A";
+                break;
+        }
+
+        char *duplexStr;
+        switch (ethernetif_get_link_duplex(netif_))
+        {
+            case kPHY_HalfDuplex:
+                duplexStr = "half";
+                break;
+            case kPHY_FullDuplex:
+                duplexStr = "full";
+                break;
+            default:
+                duplexStr = "N/A";
+                break;
+        }
+
+        PRINTF(", speed=%s_%s", speedStr, duplexStr);
+    }
+
+    PRINTF("\r\n");
+}
+
+/*!
+ * @brief Prints DHCP status of the interface when it has changed from last status.
+ *
+ * @param arg pointer to network interface structure
+ */
+static void print_dhcp_state(void *arg)
+{
+    struct netif *netif = (struct netif *)arg;
+    struct dhcp *dhcp;
+    u8_t dhcp_last_state = DHCP_STATE_OFF;
+
+    while (netif_is_up(netif))
+    {
+        dhcp = netif_dhcp_data(netif);
+
+        if (dhcp == NULL)
+        {
+            dhcp_last_state = DHCP_STATE_OFF;
+        }
+        else if (dhcp_last_state != dhcp->state)
+        {
+            dhcp_last_state = dhcp->state;
+
+            PRINTF(" DHCP state       : ");
+            switch (dhcp_last_state)
+            {
+                case DHCP_STATE_OFF:
+                    PRINTF("OFF");
+                    break;
+                case DHCP_STATE_REQUESTING:
+                    PRINTF("REQUESTING");
+                    break;
+                case DHCP_STATE_INIT:
+                    PRINTF("INIT");
+                    break;
+                case DHCP_STATE_REBOOTING:
+                    PRINTF("REBOOTING");
+                    break;
+                case DHCP_STATE_REBINDING:
+                    PRINTF("REBINDING");
+                    break;
+                case DHCP_STATE_RENEWING:
+                    PRINTF("RENEWING");
+                    break;
+                case DHCP_STATE_SELECTING:
+                    PRINTF("SELECTING");
+                    break;
+                case DHCP_STATE_INFORMING:
+                    PRINTF("INFORMING");
+                    break;
+                case DHCP_STATE_CHECKING:
+                    PRINTF("CHECKING");
+                    break;
+                case DHCP_STATE_BOUND:
+                    PRINTF("BOUND");
+                    break;
+                case DHCP_STATE_BACKING_OFF:
+                    PRINTF("BACKING_OFF");
+                    break;
+                default:
+                    PRINTF("%u", dhcp_last_state);
+                    assert(0);
+                    break;
+            }
+            PRINTF("\r\n");
+
+            if (dhcp_last_state == DHCP_STATE_BOUND)
+            {
+                PRINTF("\r\n IPv4 Address     : %s\r\n", ipaddr_ntoa(&netif->ip_addr));
+                PRINTF(" IPv4 Subnet mask : %s\r\n", ipaddr_ntoa(&netif->netmask));
+                PRINTF(" IPv4 Gateway     : %s\r\n\r\n", ipaddr_ntoa(&netif->gw));
+                STE_start_tcp_echo_server();
+            }
+        }
+
+        sys_msleep(20U);
+    }
+
+    vTaskDelete(NULL);
+}
